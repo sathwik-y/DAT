1d08a8bf1d64213ef4fd506e6415f2ac7a847cd5
sathwik-y <sathwik.yellapragada@gmail.com>
2025-08-20 23:10:17 +0530

    Send role with JWT

--- src/main/java/employee/tracker/controller/UserController.java
+++ src/main/java/employee/tracker/controller/UserController.java
@@ -1,22 +1,23 @@
 package employee.tracker.controller;
 
-import employee.tracker.model.Sales;
+import java.util.HashMap;
+import java.util.Map;
+
+import employee.tracker.enums.Role;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
 import employee.tracker.model.Users;
 import employee.tracker.service.UserService;
 import employee.tracker.utility.JwtUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -50,7 +51,14 @@ public class UserController {
         );
         String token = jwtUtil.generateToken(username);
         Map<String,String> response = new HashMap<>();
+
+        // Fetch the user
+        Users user = userService.findByUserName(username);
+        if(user==null) throw new RuntimeException("User not Found");
+
+        Role role = user.getRole();
         response.put("token",token);
+        response.put("role",role.name());
         return ResponseEntity.ok(response);
     }
 
